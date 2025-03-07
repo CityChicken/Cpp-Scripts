ee9b53ecc0cc035807ab3a8ca55a0765915d8004
Jacob Cupul <jacob.cupul@gmail.com>
2025-03-06 17:05:14 -0800

    Update cookietowns.cpp

--- cookietowns.cpp
+++ cookietowns.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define NUM_TOWNS
+#define NUM_TOWNS 100
 using namespace std;
 
 // DMOJ problem saco08p3
@@ -18,24 +18,83 @@ Output:
 struct edge{
 	int toTown;
 	int cost;
-}
+};
 
 
 struct state{
 	int cost;
 	bool solved;
-}
+};
 
-int min_array(int *array, int size){
-	int min = array[0];
-	for (int i = 1; i < size ;i++) {
-		if (array[i] < min) min = array[i];
+int min_array(int *array, int size, bool retIdx = true){
+	int idx = -1;
+	int min = -1;
+	for (int i = 0; i < size ;i++) {
+		if (min == -1) {
+			min = array[i];
+			idx = i;
+		}
+		else if (array[i] < min && array[i] > -1) {
+			min = array[i];
+			idx = i;
+		}
 	}
+	
+	if (retIdx)
+		return idx;
 	return min;
 }
 
-int shortest_path(edge *townMap[NUM_TOWNS], int numTowns, int fromTown, int toTown){
-	return 1;
+
+int min_array_state(state *array, int size, bool retIdx = true){
+	int idx = -1;
+	int min = -1;
+	for (int i = 0; i < size ;i++) {
+		if (min == -1 && array[i].solved == false ) {
+			min = array[i].cost;
+			idx = i;
+		}
+		else if (array[i].cost < min && array[i].cost > 0 && array[i].solved == false) {
+			min = array[i].cost;
+			idx = i;
+		}
+	}
+	
+	if (retIdx)
+		return idx;
+	return min;
+}
+
+int shortest_path(int townMap[NUM_TOWNS][NUM_TOWNS], state townMins[NUM_TOWNS][NUM_TOWNS],  int numTowns, int fromTown, int toTown){
+	int tempCost = 0;
+	int tempTown = fromTown;
+	int iterations = 0;
+	while (townMins[fromTown][toTown].solved == false) {
+		for (int i = 0; i < numTowns; i++) {
+			if ( i != fromTown && townMins[fromTown][i].solved == false ) {
+				int tempCostNew = tempCost + townMap[tempTown][i];
+				if ( townMins[fromTown][i].cost < 0 || tempCostNew < townMins[fromTown][i].cost) {
+					townMins[fromTown][i].cost = tempCostNew;
+				}
+			}
+		}
+		int minIdx = min_array_state(townMins[fromTown], numTowns);
+		if (minIdx > -1) {
+				townMins[fromTown][minIdx].solved = true;
+				tempCost = townMins[fromTown][minIdx].cost;
+				tempTown = minIdx;
+				cout<<"Minimum path is at town: "<<minIdx<<endl;
+		} else {
+			break;
+		}
+		iterations++;
+		if (iterations > 1000000) {
+			cout<<"Loop is not terminating"<<endl;
+			break;
+		}
+	}
+	
+	return townMins[fromTown][toTown].cost;
 }
 
 
@@ -43,13 +102,16 @@ int main(){
 	int n;
 	cin>>n;
 	int townMap[NUM_TOWNS][NUM_TOWNS];
-
+	state townMins[NUM_TOWNS][NUM_TOWNS];
+	
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++) {
 			cin>>townMap[i][j];
+			townMins[i][j].solved = false;
+			townMins[i][j].cost = -1;
 		}
 	}
-
+	
 	int m;
 	cin>>m;
 	int cookies[m];
@@ -58,10 +120,9 @@ int main(){
 		cin>>cookies[i];		
 	}
 
-
 	int totals[m];
 	for (int i = 0; i < m; i++){
-		totals[i] = shortest_path() + shortest_path() 	
+		totals[i] = shortest_path(townMap, townMins, n, 0, cookies[i] - 1) + shortest_path(townMap, townMins, n, cookies[i] - 1, n-1); 	
 	}
 
 	cout<<min_array(totals, m)<<endl;
